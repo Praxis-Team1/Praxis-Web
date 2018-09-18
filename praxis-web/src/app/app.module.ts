23931167d1e88cbc9bf91afa1ccba718bb07774c
MrPauL91 <jupvillegasgo@unal.edu.co>
2018-09-18 17:33:03 -0500

    Created directory for signUp

--- praxis-web/src/app/app.module.ts
+++ praxis-web/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StudentinfoComponent } from './components/signUp/studentinfo/studentinfo.component';
+import { RecordvideoComponent } from './components/signUp/recordvideo/recordvideo.component';
+import { ValidatedataComponent } from './components/signUp/validatedata/validatedata.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentinfoComponent,
+    RecordvideoComponent,
+    ValidatedataComponent
   ],
   imports: [
     BrowserModule
